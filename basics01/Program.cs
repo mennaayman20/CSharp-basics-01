2b4d00b44014cc3984222fe8a49821b36b43150d
menna <mennaayman200004@gmail.com>
2026-09-06 16:27:50 +0300

    apply object methods

--- basics01/Program.cs
+++ basics01/Program.cs
@@ -4,16 +4,29 @@
     {
         static void Main(string[] args)
         {
-            // Q1 Create an instance of the Book class 
+            #region Q1
+             // Create an instance of the Book class 
             Book myBook = new Book();
             myBook.Title = "the Great Gatsby";
             myBook.Pages = 200;
-
             // 2. Store it in a variable of type object
             object objBook = myBook;
-
             //print
             Console.WriteLine(objBook);
+            #endregion
+
+
+            #region Q2
+            Console.WriteLine(objBook.ToString());             
+            Console.WriteLine(objBook.Equals(objBook));       
+            Console.WriteLine(objBook.GetHashCode());          
+            Console.WriteLine(objBook.GetType());
+            #endregion
+
+            
+
+
+
 
         }
     }
